5c7d906990954dc7ae5b5a90f9cb6046fa1ee005
adegokem <adegokefoluso@gmail.com>
2022-05-04 09:50:14 -0400

    cluster tested locally

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=adegokem/flaskapp
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker tag project $dockerpath
+dcoker login --username=adegokem
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
